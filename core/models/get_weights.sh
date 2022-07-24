056c5ef7f6f592d075ef885a321a0d4a2078e155
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2022-07-24 20:07:18 +0530

    added get_weights.sh

---
+++ core/models/get_weights.sh
@@ -0,0 +1,4 @@
+echo "Downloading weights from google drive..."
+cd core/models
+gdown --folder 'https://drive.google.com/drive/folders/1u14RE8zgpFyKfXugVZY0R2qT_iHteOIx?usp=sharing'
+gdown --folder 'https://drive.google.com/drive/folders/1VaQ1VVZxNsWq5W8jeFmhlf1YpD4uI5D-?usp=sharing'
